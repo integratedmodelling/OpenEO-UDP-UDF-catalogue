1432321117a971e124de5681848b7fef0a8edda3
Dr. Marcel Buchhorn <marcel.buchhorn@vito.be>
2023-05-16 15:26:36 +0200

    test of UDF functions

--- UDF/UDF_remapping.py
+++ UDF/UDF_remapping.py
@@ -1,18 +1,24 @@
-from openeo.udf import XarrayDataCube, inspect
+import numpy
+from openeo.udf import XarrayDataCube
+from openeo.processes import nan
+import xarray
+
 
 def apply_datacube(cube: XarrayDataCube, context: dict) -> XarrayDataCube:
     """ remap cube values by given dictionary
 
-    Note: currently implace the values
-
     :param cube: data cube
     :param context: dictionary to provide external data - key class_mapping is used to provide external re-mapping dict
-    :return: remapped data cube
+    :return: data cube with remapped values (new cube)
     """
     class_mapping = context["class_mapping"]
-    array = cube.get_array()
+
+    array: xarray.DataArray = cube.get_array()
+    result = numpy.full_like(array.values, nan())
+
     for k, v in class_mapping.items():
         # Note: JSON-encoding of the class mapping converted the keys from int to strings, so we have to undo that
         k = int(k)
-        array.values[array.values == k] = v
-    return cube
+        result[array.values == k] = v
+
+    return XarrayDataCube(array=xarray.DataArray(result, dims=array.dims, coords=array.coords))
