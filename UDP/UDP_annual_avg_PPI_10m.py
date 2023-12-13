537060676e39e06c69edd894d84c8f6c2b1d2e18
Dr. Marcel Buchhorn <marcel.buchhorn@vito.be>
2023-12-13 10:22:19 +0100

    NCAP-1150 adding the PPI Tier-2 variable

---
+++ UDP/UDP_annual_avg_PPI_10m.py
@@ -0,0 +1,93 @@
+"""
+UDP to generate an annual average PPI in high resolution (10m) from the Copernicus
+High Resolution Vegetation Phenology and Productivity (HR-VPP) product suite
+
+"""
+import json
+import openeo
+from openeo.api.process import Parameter
+from openeo.processes import if_, and_, gte, add, text_concat
+from openeo.rest.udp import build_process_dict
+import os
+import pathlib
+
+# Establish connection to OpenEO instance (note that authentication is not necessary to just build the UDP)
+connection = openeo.connect(url="openeo.vito.be")
+
+# set up the UDP parameters
+param_geo = Parameter(
+    name="geometry",
+    description="Geometry as GeoJSON feature(s).",
+    schema={"type": "object", "subtype": "geojson"},
+)
+
+param_year = Parameter.integer(
+    name="year",
+    default=2021,
+    description="The year for which to generate an annual mean composite. from 2017 onwards.",
+)
+
+param_warp = Parameter.boolean(
+    name="output_warp",
+    default=False,
+    description="Boolean switch if output should be warped to given projection and resolution, default=False.",
+)
+
+param_epsg = Parameter.integer(
+    name="output_epsg",
+    default=3035,
+    description="The desired output projection system, which is EPSG:3035 by default.",
+)
+
+param_resolution = Parameter.number(
+    name="resolution",
+    default=10,
+    description="The desired resolution, specified in units of the projection system, which is meters by default.",
+)
+
+start = text_concat([param_year, "01", "01"], separator="-")
+end = text_concat([add(param_year, 1), "01", "01"], separator="-")
+
+cube = connection.load_collection(
+    "SEASONAL_TRAJECTORIES",
+    temporal_extent=[start, end],
+    bands=["PPI"]
+)
+
+# masking to valid data and rescaling
+cube = cube.apply(lambda x: if_(and_(x >= 0, x <= 30000), x / 10000.0))
+
+# reduce the temporal dimension with mean reducer
+cube = cube.reduce_dimension(dimension="t", reducer=lambda data: data.mean())
+
+# warp to specified projection and resolution if needed
+cube_resample = cube.resample_spatial(resolution=param_resolution, projection=param_epsg, method="bilinear")
+cube = if_(param_warp, cube_resample, cube)
+
+# filter spatial by BBOX given in the specified EPSG
+cube = cube.filter_spatial(geometries=param_geo)
+
+description = """
+Given a year and area of interest, returns an annual mean composite of PPI (https://collections.eurodatacube.com/seasonal-trajectories/).
+"""
+
+spec = build_process_dict(
+    process_id="udp_annual_avg_ppi_10m",
+    summary="Annual mean composite of Copernicus PPI. Returns a single band RasterCube.",
+    description=description.strip(),
+    parameters=[
+        param_geo,
+        param_year,
+        param_warp,
+        param_epsg,
+        param_resolution,
+    ],
+    process_graph=cube,
+)
+
+# dump to json file to be usable as UDP
+this_script = pathlib.Path(__file__)
+json_file = os.path.normpath(os.path.join(this_script.parent, 'json', this_script.name.lower().split('.')[0] + '.json'))
+print(f"Writing UDP to {json_file}")
+with open(json_file, "w", encoding="UTF8") as f:
+    json.dump(spec, f, indent=2)
